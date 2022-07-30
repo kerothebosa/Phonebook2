8ba65d3614cc4656503f2219dad528a15b0eef80
kerothebosa <100617243+kerothebosa@users.noreply.github.com>
2022-07-30 20:03:10 +0300

    Update Book.cs
    final

--- Phonebook/Book.cs
+++ Phonebook/Book.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,96 +12,45 @@ namespace Phonebook
         private void DisplayDetails(Contact contact)
         {
 
-        Console.WriteLine($"Bulunan kayıt: İsim:{contact.Name}, Soyisim:{contact.Surname}, Numara:{contact.Number}");
+            Console.WriteLine($"Bulunan kayıt: İsim:{contact.Name}, Soyisim:{contact.Surname}, Numara:{contact.Number}");
         }
-
-        
-       
-
 
 
         private void DisplayDetails(List<Contact> contacts)
         {
-            
+
             foreach (var contact in contacts)
             {
                 DisplayDetails(contact);
             }
-
-
         }
-
-       /*
-        * Deneme
-        * 
-        * 
-        * private void DisplayAlpha(Contact alpha)
-        {
-
-            Console.WriteLine($"Bulunan kayıt: İsim:{alpha.Name}, Soyisim:{alpha.Surname}, Numara:{alpha.Number}");
-        }
-        private void DisplayAlpha(List<Contact> contacts)
-        {
-            contacts = contacts.OrderBy(x => x.Name).ToList();
-
-            foreach (var alpha in contacts)
-            {
-                DisplayAlpha(alpha);
-
-            }
-
-
-        }
-    */
+     
         public void AlphaName()
         {
-
-            var contact= _contacts.OrderBy(x => x.Name).ToList();
+            var contact = _contacts.OrderBy(x => x.Name).ToList();
             DisplayDetails(contact);
         }
         public void AlphaSurname()
         {
-
             var contact = _contacts.OrderBy(x => x.Surname).ToList();
             DisplayDetails(contact);
         }
 
         public void AlphaNumber()
         {
-
             var contact = _contacts.OrderBy(x => x.Number).ToList();
             DisplayDetails(contact);
         }
 
-
         public void AddContact(Contact contact)
         {
-        _contacts.Add(contact);
+            _contacts.Add(contact);
         }
 
-        public void Already(string name, string surname)
+        public bool Already(string name, string surname)
         {
-            var already = _contacts.Any(x => x.Name == name && x.Surname == surname);
-            
-            if(already== true) 
-            { 
-                Console.WriteLine("Aynı İsim ve Soyisimli bir kayıt var.");
-                Console.WriteLine("Kayıdın üzerine yeni numara yazmak için 1 yazınız");
-                Console.WriteLine("Ana menüye dönmek için 2 yazınız ");
-                var UserInput4 = Console.ReadLine();
-                if (UserInput4 == "1")
-                {
-                    
-                }
-               
-            }
-            else
-            {
-                
-            }
+            return _contacts.Any(x => x.Name.ToLower() == name.Trim().ToLower() && x.Surname.ToLower() == surname.Trim().ToLower());
         }
-
-
 
         public void DisplayContact(string number)
         {
@@ -125,14 +74,14 @@ namespace Phonebook
         {
             if (_contacts.Count == 0)
             {
-            Console.WriteLine("Kayıt bulunamadı");
+                Console.WriteLine("Kayıt bulunamadı");
             }
             else
             {
-            DisplayDetails(_contacts);
+                DisplayDetails(_contacts);
             }
-                    
-         
+
+
         }
         public void DisplayMatched(string search)
         {
@@ -166,8 +115,20 @@ namespace Phonebook
             }
         }
 
+        public void DeleteContact(string name, string surname, string number)
+        {
 
-        
+            var personToBeDeleted = _contacts.FirstOrDefault(x => x.Name.ToLower() == name.Trim().ToLower() && x.Surname.ToLower() == surname.Trim().ToLower());
+
+            _contacts.Remove(personToBeDeleted);
+            Contact _contact = new Contact(name, surname, number);
+            _contacts.Add(_contact);
+
+
+        }
+
+
+
 
     }
 }
